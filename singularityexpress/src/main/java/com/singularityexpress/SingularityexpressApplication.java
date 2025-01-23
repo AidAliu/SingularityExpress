7f3ac42c8657de39b587adb449b4e15abaddfcf0
AidAliu <aidaliu50@gmail.com>
2025-01-23 19:26:34 +0100

    Spring boot installed

---
+++ singularityexpress/src/main/java/com/singularityexpress/SingularityexpressApplication.java
@@ -0,0 +1,13 @@
+package com.singularityexpress;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SingularityexpressApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SingularityexpressApplication.class, args);
+	}
+
+}
---
+++ singularityexpress/src/test/java/com/singularityexpress/SingularityexpressApplicationTests.java
@@ -0,0 +1,13 @@
+package com.singularityexpress;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SingularityexpressApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
